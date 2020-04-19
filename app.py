f22dacdd3494b4491e9f879ca8165b2046ebdb86
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-19 23:07:10 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -10,3 +10,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/prediction', methods='POST')
+def prediction():
+    rm = request.form['RM']
+    lstat = request.form['LSTAT']
+    predict = model.predict([rm, lstat])
+    return render_template('predicted.html', rm=rm, lstat=lstat, predict=predict)
