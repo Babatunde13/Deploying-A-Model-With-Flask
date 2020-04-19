9655377b20df51277663e91df02c67c98fa12e60
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-19 23:24:19 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def home():
         rm = request.form['RM']
         lstat = request.form['LSTAT']
         predict = model.predict([rm, lstat])
-        return url_for('prediction', rm=rm, lstat=lstat, predict=predict)
+        return render_template(url_for('prediction', rm=rm, lstat=lstat, predict=predict))
         # return render_template('predicted.html', rm=rm, lstat=lstat, predict=predict)
 
     return render_template('home.html')
