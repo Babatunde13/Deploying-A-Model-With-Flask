e435594e9d9639b129dcd0bfb583f359f1931858
Babatunde13 <koikibabatunde14@gmail.com>
2020-04-20 18:45:30 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/prediction')
 def prediction():
-    # rm, lstat, predict = (1, 1, 1)
+    
     return render_template('predicted.html', rm=1, lstat=1, predict=1)
 
 
@@ -15,12 +15,12 @@ def prediction():
 @app.route('/home/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        print(1)
-        # rm = request.form['RM']
-        # lstat = request.form['LSTAT']
-        # predict = model.predict([rm, lstat])
+       
+        rm = request.form['RM']
+        lstat = request.form['LSTAT']
+        predict = model.predict([rm, lstat])
         # print(predict)
         # return render_template(url_for('prediction', rm=rm, lstat=lstat, predict=predict))
-        return render_template('predicted.html', rm=1, lstat=2, predict=3)
+        return render_template('predicted.html', rm=rm, lstat=lstat, predict=predict)
 
     return render_template('home.html')
